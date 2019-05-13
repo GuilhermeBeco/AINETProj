3949a0fe0850649b7a370c11f2b3870989480284
brychka <petro_brychka@hotmail.com>
2019-05-13 15:18:57 +0100

    US1 - testes

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-use App\Http\Controllers\Auth\Request;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 
 class LoginController extends Controller
@@ -43,7 +44,7 @@ class LoginController extends Controller
     {
         if ($user->ativo == 0) {// do your margic here
             Auth::logout();
-            return redirect()->route('login')->with('error', 'Tem que estar ativo');
+            return redirect()->route('login');
         }
     }
 
